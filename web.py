f0e0f29263f0fa2122e26cb9c97c59034678e692
mrKeft <ktapps19@gmail.com>
2025-01-21 13:16:30 -0400

    Updated look of app

--- web.py
+++ web.py
@@ -4,18 +4,20 @@ import functions as funcs
 
 todos = funcs.get_todos()
 
-
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     funcs.write_todos(todos)
 
 
-
 st.title("My Todo App")
-st.subheader("This is my todo app")
+st.subheader("Welcome.")
 
-st.write("This app is to increase productivity")
+st.write("This app aims to increase <b>productivity</b>", unsafe_allow_html=True )
+
+st.text_input(label="temp", label_visibility="hidden", placeholder="Add new todo...", on_change=add_todo,
+              key='new_todo')
+#st.session_state["new_todo"] = ""
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -25,9 +27,4 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun(scope="app")
         #print("reurn")
-
-
-st.text_input(label="temp", label_visibility="hidden", placeholder="Add new todo...", on_change=add_todo,
-              key='new_todo')
-
 #st.session_state
